ed5dcfe2a466bde8632c61370fb6c786033c2116
d4xika <katja.daxi@gmail.com>
2025-05-25 18:16:48 +0200

    error with pausing fixed

--- src/views/mainView.vue
+++ src/views/mainView.vue
@@ -3,8 +3,8 @@
 	<div>
 		<div style="display: flex; justify-content: center; align-items: center; flex-direction: column">
 			<div>
-				<img v-if="this.timer.running" style="height: 200px; margin-bottom: 5px" src="/images/workingKitty.gif" alt="working kitty" />
-				<img v-else style="height: 160px; margin-top: 45px" src="/images/sleepingKitty.GIF" alt="sleeping kitty" />
+				<img v-if="this.timer.running" style="height: 200px; margin-bottom: 5px; margin-top: 50px" src="/images/workingKitty.gif" alt="working kitty" />
+				<img v-else style="height: 160px; margin-top: 95px" src="/images/sleepingKitty.GIF" alt="sleeping kitty" />
 			</div>
 			<div id="timerBox">
 				<p>{{this.timer.display}}</p>
@@ -38,80 +38,100 @@ export default {
 				passedTime: null,
 				running: false,
 				display: "00:00",
-				finished: false
+				finished: false,
+				elapsedBeforePause: 0,
 			},
 			intervalId: null
 		}
 	},
 	methods: {
-		changeTimerFormat() {
-			if (!this.timer.startTime) return;
-
-			let passed = Math.floor((Date.now() - this.timer.startTime) / 1000);
-			let minutes = String(Math.floor(passed / 60)).padStart(2, '0');
-			let seconds = String(passed % 60).padStart(2, '0');
-			this.timer.display = `${minutes}:${seconds}`;
-		},
-
 		startTimer() {
-			if(this.timer.running) return
+			if (this.timer.running) return;
 
-			this.timer.running = true
-			this.timer.startTime = Date.now()
+			const now = Date.now();
+
+			this.timer.startTime = now - this.timer.elapsedBeforePause * 1000;
+			this.timer.running = true;
+			this.timer.finished = false;
 
 			localStorage.setItem('timerStartTime', this.timer.startTime);
 			localStorage.setItem('timerRunning', 'true');
+			localStorage.setItem('elapsedBeforePause', this.timer.elapsedBeforePause);
 
 			this.intervalId = setInterval(() => {
-				this.changeTimerFormat();
+				this.updateTimerDisplay();
 			}, 1000);
 
-
-			this.changeTimerFormat()
-
+			this.updateTimerDisplay();
 		},
 
 		pauseTimer() {
-			this.timer.running = false
-			clearInterval(this.intervalId)
+			if (!this.timer.running) return;
+
+			this.timer.running = false;
+			clearInterval(this.intervalId);
+
+			this.timer.elapsedBeforePause = Math.floor((Date.now() - this.timer.startTime) / 1000);
+
+			localStorage.setItem('elapsedBeforePause', this.timer.elapsedBeforePause);
+			localStorage.setItem('timerRunning', 'false');
+			localStorage.removeItem('timerStartTime');
 		},
 
 		resetTimer() {
 			this.pauseTimer();
+
 			this.timer.startTime = null;
+			this.timer.elapsedBeforePause = 0;
 			this.timer.display = "00:00";
 			this.timer.finished = false;
 
 			localStorage.removeItem('timerStartTime');
+			localStorage.removeItem('elapsedBeforePause');
 			localStorage.removeItem('timerRunning');
 		},
 
 		stopTimer() {
-			this.pauseTimer();
+			this.resetTimer();
 			this.timer.finished = true;
-			this.timer.display = "00:00";
+		},
 
-			localStorage.removeItem('timerStartTime');
-			localStorage.removeItem('timerRunning');
+		updateTimerDisplay() {
+			if (this.timer.running && this.timer.startTime) {
+				const elapsed = Math.floor((Date.now() - this.timer.startTime) / 1000);
+				this.timer.elapsedBeforePause = elapsed;
+				this.timer.display = this.formatElapsed(elapsed);
+
+				localStorage.setItem('elapsedBeforePause', elapsed);
+			}
+		},
+
+		formatElapsed(seconds) {
+			const mins = String(Math.floor(seconds / 60)).padStart(2, '0');
+			const secs = String(seconds % 60).padStart(2, '0');
+			return `${mins}:${secs}`;
 		}
-
 	},
 
 	mounted() {
-		const savedStartTime = localStorage.getItem('timerStartTime');
 		const wasRunning = localStorage.getItem('timerRunning') === 'true';
+		const savedStartTime = localStorage.getItem('timerStartTime');
+		const savedElapsed = localStorage.getItem('elapsedBeforePause');
 
-		if (savedStartTime && wasRunning) {
+		this.timer.elapsedBeforePause = savedElapsed ? parseInt(savedElapsed) : 0;
+
+		if (wasRunning && savedStartTime) {
 			this.timer.startTime = parseInt(savedStartTime);
 			this.timer.running = true;
 
 			this.intervalId = setInterval(() => {
-				this.changeTimerFormat();
+				this.updateTimerDisplay();
 			}, 1000);
 
-			this.changeTimerFormat();
+			this.updateTimerDisplay();
 		} else {
-			this.resetTimer();
+			this.timer.running = false;
+			this.timer.display = this.formatElapsed(this.timer.elapsedBeforePause);
 		}
 	}
 }
