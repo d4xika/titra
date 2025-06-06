6e7dcf6a39957ba8da445d51c06f1786d5894e19
d4xika <katja.daxi@gmail.com>
2025-06-06 12:59:52 +0200

    icon color updated

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
     iconPaths: {
-      favicon32: 'img/icons/kittyIconGreen_192x192.png',
-      favicon16: 'img/icons/kittyIconGreen_192x192.png',
-      appleTouchIcon: 'img/icons/kittyIconGreen_192x192.png',
-      maskIcon: 'img/icons/kittyIconGreen_192x192.png',
-      msTileImage: 'img/icons/kittyIconGreen_192x192.png'
+      favicon32: 'img/icons/kittyIconBlue_192x192.png',
+      favicon16: 'img/icons/kittyIconBlue_192x192.png',
+      appleTouchIcon: 'img/icons/kittyIconBlue_192x192.png',
+      maskIcon: 'img/icons/kittyIconBlue_192x192.png',
+      msTileImage: 'img/icons/kittyIconBlue_192x192.png'
     }
   }
 })
